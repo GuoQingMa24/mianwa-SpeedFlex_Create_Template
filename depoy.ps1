fabadbd8f19a45396cd1d9dfd79a2e13a17830ce
cqupt-pengyuyan <116574444+GuoQingMa24@users.noreply.github.com>
2025-03-16 12:34:09 +0800

    fix-脚本在windows环境下无法运行

--- depoy.ps1
+++ depoy.ps1
@@ -3,27 +3,24 @@ $REPO_URL = "git@github.com:GuoQingMa24/mianwa-SpeedFlex_Create_Template.git"
 $BUILD_DIR = "dist"
 $BRANCH = "main"
 
-Write-Host "正在打包前端项目..."
+Write-Host "Building..."
 npm run build
 if ($LASTEXITCODE -ne 0) {
-    Write-Host "打包失败，请检查错误！"
+    Write-Host "Build fail！"
     exit 1
 }
 
-Write-Host "进入构建目录 $BUILD_DIR..."
+Write-Host "cd dist $BUILD_DIR..."
 Set-Location $BUILD_DIR
-if (-not $?) {
-    Write-Host "无法进入 $BUILD_DIR，目录不存在！"
-    exit 1
-}
 
-Write-Host "准备上传到 GitHub..."
+
+Write-Host "Ready to push GitHub..."
 git add .
-git commit -m "Deploy: 更新打包文件 $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+git commit -m "Deploy: update tarball $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 git push origin $BRANCH
 if ($LASTEXITCODE -eq 0) {
-    Write-Host "成功部署到 GitHub!"
+    Write-Host "Success deploy GitHub!"
 } else {
-    Write-Host "推送失败，请检查 SSH 配置或网络连接！"
+    Write-Host "Fail,please check out the Network！"
     exit 1
 }
